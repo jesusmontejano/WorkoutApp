864607bfdcbbe27fbf563bb6d4856bb17e5bf448
jesusmontejano <66389093+jesusmontejano@users.noreply.github.com>
2023-01-26 08:42:36 -0800

    Add files via upload

---
+++ database_service.py
@@ -0,0 +1,42 @@
+import harperdb
+
+url = "https://cloud-1-jesusmontejano.harperdbcloud.com"
+username = "jesus"
+password = "Horhay1994!"
+
+db = harperdb.HarperDB(
+    url=url,
+    username=username,
+    password=password
+)
+
+SCHEMA = "workout_repo"
+TABLE = "workouts"
+TABLE_TODAY = "workout_today"
+
+data = {
+    "video_id": "123",
+    "title": "Test 1"
+}
+
+def insert_workout(workout_data):
+    return db.insert(SCHEMA, TABLE, [workout_data])
+
+def delete_workout(workout_id):
+    return db.delete(SCHEMA, TABLE, [workout_id])
+
+def get_all_workouts():
+    try:
+        return db.sql(f"select video_id,channel,title,duration from {SCHEMA}.{TABLE}")
+    except harperdb.exceptions.HarperDBError:
+        return []
+
+def get_workout_today():
+    return db.sql(f"select * from {SCHEMA}.{TABLE} where id = 0")
+
+def update_workout_today(workout_data, insert=False):
+    workout_data['id'] = 0
+    if insert:
+        return db.insert(SCHEMA, TABLE_TODAY, [workout_data])
+    return db.update(SCHEMA, TABLE_TODAY, [workout_data])
+
